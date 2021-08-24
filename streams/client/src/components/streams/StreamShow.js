fab955d065d15641eefa23398cf01c5a0c9f4410
ConradMare890317 <Conradmare@gmail.com>
2021-08-24 12:07:46 +0200

    2021/08/24 (am)
    Signed-off-by: ConradMare890317 <Conradmare@gmail.com>

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,80 +1,49 @@
 import React from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { fetchStreams } from '../../actions';
+import { fetchStream } from '../../actions';
 
-class StreamList extends React.Component {
+class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
-    this.props.fetchStreams();
-  }
+    const { id } = this.props.match.params;
 
-  renderAdmin(stream) {
-    if (stream.userId === this.props.currentUserId) {
-      return (
-        <div className="right floated content">
-          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
-            Edit
-          </Link>
-          <Link
-            to={`/streams/delete/${stream.id}`}
-            className="ui button negative"
-          >
-            Delete
-          </Link>
-        </div>
-      );
-    }
-  }
-
-  renderList() {
-    return this.props.streams.map(stream => {
-      return (
-        <div className="item" key={stream.id}>
-          {this.renderAdmin(stream)}
-          <i className="large middle aligned icon camera" />
-          <div className="content">
-            <Link to={`/streams/${stream.id}`} className="header">
-              {stream.title}
-            </Link>
-            <div className="description">{stream.description}</div>
-          </div>
-        </div>
-      );
+    this.props.fetchStream(id);
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`
     });
-  }
-
-  renderCreate() {
-    if (this.props.isSignedIn) {
-      return (
-        <div style={{ textAlign: 'right' }}>
-          <Link to="/streams/new" className="ui button primary">
-            Create Stream
-          </Link>
-        </div>
-      );
-    }
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
+    const { title, description } = this.props.stream;
+
     return (
       <div>
-        <h2>Streams</h2>
-        <div className="ui celled list">{this.renderList()}</div>
-        {this.renderCreate()}
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
+        <h1>{title}</h1>
+        <h5>{description}</h5>
       </div>
     );
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    streams: Object.values(state.streams),
-    currentUserId: state.auth.userId,
-    isSignedIn: state.auth.isSignedIn
-  };
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
 export default connect(
   mapStateToProps,
-  { fetchStreams }
-)(StreamList);
+  { fetchStream }
+)(StreamShow);
